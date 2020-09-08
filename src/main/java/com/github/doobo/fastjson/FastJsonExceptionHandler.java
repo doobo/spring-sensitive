f62fd3d91d97e46b48e24b07919e7575e5caa77c
doobo <doobo@foxmail.com>
2020-09-08 21:01:16 +0800

    拦截优先级处理

--- src/main/java/com/github/doobo/fastjson/FastJsonExceptionHandler.java
+++ src/main/java/com/github/doobo/fastjson/FastJsonExceptionHandler.java
@@ -2,20 +2,20 @@ package com.github.doobo.fastjson;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  *  脱敏方法异常处理
  */
-@ControllerAdvice
 @Slf4j
+@Order(-1)
+@RestControllerAdvice
 public class FastJsonExceptionHandler {
     
-    @ResponseBody
     @ExceptionHandler(value = {FastJsonCustomException.class})
     public ResponseEntity<Object> exception(FastJsonCustomException e) {
         if(e != null) {
