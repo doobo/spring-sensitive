57191a536c365382d5c460797f6013e9c7aac181
doobo <doobo@foxmail.com>
2025-10-29 09:17:25 +0800

    发布脚本更新

--- install.bat
+++ install.bat
@@ -2,7 +2,7 @@
 :: 设置 JAVA_HOME（Windows 路径使用反斜杠 \）
 set JAVA_HOME="C:\users\Administrator\.jdks\ms-21.0.7"
 :: 添加到 PATH
-set PATH=%PATH%;%JAVA_HOME%\bin
+set PATH=%JAVA_HOME%\bin;%PATH%
 
 :: 验证变量
 echo JAVA_HOME: %JAVA_HOME%
