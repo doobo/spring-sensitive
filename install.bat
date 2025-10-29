cceb3a5cb80aad681e4d1b8b5ca54678b3aded9f
doobo <doobo@foxmail.com>
2025-10-29 09:08:37 +0800

    发布脚本更新

--- install.bat
+++ install.bat
@@ -1,3 +1,15 @@
+@echo off
+:: 设置 JAVA_HOME（Windows 路径使用反斜杠 \）
+set JAVA_HOME="C:\users\Administrator\.jdks\ms-21.0.7"
+:: 添加到 PATH
+set PATH=%PATH%;%JAVA_HOME%\bin
+
+:: 验证变量
+echo JAVA_HOME: %JAVA_HOME%
+java -version
+:: powershell设置的环境变量
+:: $env:JAVA_HOME = "C:/users/Administrator/.jdks/ms-21.0.7"
+:: $env:Path += ";$env:JAVA_HOME\bin"
 CALL mvn clean
 CALL mvn release:prepare
 CALL mvn release:perform
